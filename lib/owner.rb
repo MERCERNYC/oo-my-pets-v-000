fe208cf9c929cfe62c568e20484a129bb3d9efd1
Juliana Fogg <julianafogg@gmail.com>
2017-10-12 02:06:51 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -69,8 +69,8 @@ class Owner
     @pets.each do |type, pets|
     pets. each do |pet| pet.mood = "nervous"}
   end
-  @pets[type].clear
-  end 
+  # @pets[type].clear
+  # end 
 
   def list_pets
     "I have #{@pets[:fishes].count} fish, #{@pets[:dogs].count} dog(s), and #{@pets[:cats].count} cat(s)."
