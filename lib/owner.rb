5de410c6936387cb8e1e4a57a4f4e0a7064a19b5
Juliana Fogg <julianafogg@gmail.com>
2017-10-12 01:18:54 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -66,7 +66,7 @@ class Owner
   end
 
   def sell_pets
-    @pets.each {|type, pets| pets.each {|pet| pet.mood = "nervous"}
+    @pets.each { |type, pets|pets.each { |pet|pet.mood = "nervous"}
   end
 
   def list_pets
