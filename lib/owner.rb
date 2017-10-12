f23c048cce51d21ad933ee63b156ee48fa182eb8
Juliana Fogg <julianafogg@gmail.com>
2017-10-12 02:15:47 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -66,12 +66,10 @@ class Owner
   end
 
   def sell_pets
-    @pets.each do |type, pets| #(Value=pets)iterate over an hash
-    pets. each do |pet| pet.mood = "nervous" #iterate over an array and change the mood
+    @pets.each {|type, pets| pets. each do |pet| pet.mood = "nervous" } #(Value=pets)iterate over an hash
+     #iterate over an array and change the mood
   end
-end
   # @pets[type].clear
-  end
 
   def list_pets
     "I have #{@pets[:fishes].count} fish, #{@pets[:dogs].count} dog(s), and #{@pets[:cats].count} cat(s)."
